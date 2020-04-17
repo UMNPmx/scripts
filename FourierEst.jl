5d3f6c0f73cd92ce0a3a3e7660b16f2fa0b903aa
Mu'taz M. Jaber <jaber038@umn.edu>
2020-04-16 22:54:42 -0500

    Create FourierEst.jl

---
+++ FourierEst.jl
@@ -0,0 +1,67 @@
+
+#--------------------------------------------------
+# Julia program for Fourier estimation method     #
+#--------------------------------------------------
+# Author: Mutaz Jaber © 2020 <jaber038@umn.edu>   #
+# GitHub: Mutaz94                                 #
+#--------------------------------------------------
+# different harmonics will be used and it depends #
+# on the L2-approximation algorithm for the       #
+# Adjustment                                      #
+#--------------------------------------------------
+# Equation:                                       #
+# R(t)= a₀ + ∑ a_n *cos(2nπt/24)+b_n*sin(2nπt/24) #
+# n: harmonic number, a,b: Fourier coeff          #
+#-------------------------------------------------$
+
+using LinearAlgebra, Optim, Plots
+using CSV
+
+# this is one harmonic model just for testing the optimizer
+# delete this after the run.
+# Importing some data to test
+df = CSV.read("./Documents/R_projects/HydroC-Precision/HydroC-Pump/health.csv")
+
+time = convert(Array, df.TIME)
+Conc = convert(Array, df.AVG)
+
+using LsqFit
+res = fit.resid
+
+ function fourEst!(t,p)
+            a₀, a_1, b_1, a_2, b_2 = p
+            a₀ .+ a_1*cos.((2*π.*t)/24) .+ b_1*sin.((2*π.*t)/24) +
+                 a_2*cos.((2*π.*t)/12) .+ b_2*sin.((2*π.*t)/12)
+end
+
+init = [0.1, 0.2, 0.1, 1, 1]
+
+
+fit2 = curve_fit(fourEst!, time, Conc, init)
+est = fit2.param
+pred = fourEst!(time, est)
+
+using Plots; plotly()
+plot(time, pred)
+plot!(time, Conc)
+scatter(time, res)
+
+# L2 approximation function to check the percentage
+# of each harmonic.
+
+function L2_approx(est)
+    a₀, a₁, b₁, a₂, b₂ = est
+    R₀ = a₀
+    R₁ = sqrt((a₁)^2 + (b₁)^2)
+    R₂ = sqrt((a₂)^2 + (b₂)^2)
+    Rn = R₀ + R₁ + R₂
+
+    H₀ = (2 *(R₀)^2/(2 *(R₀)^2 + Rn^2))*100
+    H₁ = ((R₁)^2/(2 *(R₀)^2 + Rn^2))*100
+    H₂ = ((R₂)^2/(2 *(R₀)^2 + Rn^2))*100
+    println("p₀ = $H₀", "\n p₁ = $H₁", "\n p₂ = $H₂")
+    return  nothing
+end
+
+percentage = L2_approx(est)
+
